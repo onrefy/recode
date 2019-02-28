e230b8e4fde5f3056735f9af3cb1c88e5eeb1edd
onrefy <673462121@qq.com>
2019-02-28 17:25:02 +0800

    两点值得注意：1.js中的数组和对象都不能直接拷贝2.递归注意要有值去承载函数

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
+'use strict'
+class Link {
+    constructor(value, status = true, possibleArray = [], choose = null) {
+        this.value = value;
+        this.status = status;
+        this.possibleArray = possibleArray;
+        this.choose = choose;
+    }
+    deepestChoose(l) {
+        if (l.choose == null) {
+            return l;
+        } else {
+            let next = this.deepestChoose(l.choose);
+            if (next){
+                return next;
+            }
+        }
+    }
+}
 var dotArray = [];
 var bound = [];
-var cellSize;
+var curves = [];
+var cellSize = 100;
 var xSize = 5,
     ySize = 5;
+var number = 1;
+
+var lineWidth = 5;
+var dotWidth = 2;
 for (let i = 0; i < xSize; i++) {
     let tempArray = [];
     for (let j = 0; j < ySize; j++) {
         tempArray.push(false);
-        if (j == 0 || i == 0) {
+        if ((j == 0 ||
+                i == 0 ||
+                j == ySize - 1 ||
+                i == xSize - 1) &&
+            !(i == 0 && j == 0) &&
+            !(i == 0 && j == ySize - 1) &&
+            !(i == xSize - 1 && j == 0) &&
+            !(i == xSize - 1 && j == ySize - 1)
+        ) {
             bound.push({
                 x: i,
                 y: j
@@ -24,119 +56,241 @@ class oneCurve {
             x: startX,
             y: startY
         }
+        dotArray[startX][startY] = true;
         this.keyPt = {
             x: startX,
             y: startY
         };
         this.directionPassed = [];
-
+        this.linkMap = new Link(0);
+        this.curveIndex = curves.length;
         while (!this.end) {
             this.update();
         }
+        curves.push(this);
     }
     update() {
         var nextDirection;
-        if (this.startPt.x == this.keyPt.x && this.startPt.y == this.keyPt.y) {
+        var preDirection;
+
+        if (this.directionPassed.length == 0) {
             if (this.keyPt.x == 0) {
-                nextDirection = 0;
+                preDirection = 0;
             } else {
                 if (this.keyPt.x == xSize - 1) {
-                    nextDirection = 6;
+                    preDirection = 6;
                 } else {
                     if (this.keyPt.y == 0) {
-                        nextDirection = 9;
+                        preDirection = 3;
                     } else {
                         if (this.keyPt.y == ySize - 1) {
-                            nextDirection = 3;
+                            preDirection = 9;
                         }
                     }
                 }
             }
         } else {
-            console.log(this.directionAround(0));
-            nextDirection = this.directionAround(this.directionPassed[this.directionPassed.length - 1])[Math.floor(Math.random()*3)];
+            preDirection = this.directionPassed[this.directionPassed.length - 1];
         }
+        this.directionAround(preDirection).forEach((value) => {
+            this.linkMap.deepestChoose(this.linkMap).possibleArray.push(new Link(value));
+        });
+
+        nextDirection = this.directionAround(preDirection)[Math.floor(Math.random() * this.directionAround(preDirection).length)];
+
+
+        let a = this.linkMap.deepestChoose(this.linkMap);
+        a.choose = new Link(3);
+        console.log(this.linkMap);
+
+
+
+
+
 
         this.directionPassed.push(nextDirection);
-        switch (nextDirection) {
-            case 0:
-                this.keyPt.x++;
-                break;
-            case 1:
-                this.keyPt.x++;
-                this.keyPt.y++;
-                break;
-            case 2:
-                this.keyPt.x++;
-                this.keyPt.y++;
-                break;
-            case 3:
-                this.keyPt.y++;
-                break;
-            case 4:
-                this.keyPt.y++;
-                this.keyPt.x--;
-                break;
-            case 5:
-                this.keyPt.y++;
-                this.keyPt.x--;
-                break;
-            case 6:
-                this.keyPt.x--;
-                break;
-            case 7:
-                this.keyPt.x--;
-                this.keyPt.y--;
-                break;
-            case 8:
-                this.keyPt.x--;
-                this.keyPt.y--;
-                break;
-            case 9:
-                this.keyPt.y--;
-                break;
-            case 10:
-                this.keyPt.x++;
-                this.keyPt.y--;
-                break;
-            case 11:
-                this.keyPt.x++;
-                this.keyPt.y--;
-        }
+
+
+
+
+
+
+        this.moveByIndex(this.keyPt, nextDirection);
+        dotArray[this.keyPt.x][this.keyPt.y] = true;
+
         if (this.keyPt.x == 0 || this.keyPt.y == 0 || this.keyPt.x == xSize - 1 || this.keyPt.y == ySize - 1) {
             this.end = true;
         }
     }
-    render() {}
+
+    render() {
+        var present = {
+            x: this.startPt.x,
+            y: this.startPt.y
+        };
+        this.directionPassed.forEach((value) => {
+            var old = {
+                x: present.x,
+                y: present.y
+            };
+            noFill();
+            strokeWeight(lineWidth);
+            strokeCap(SQUARE);
+            this.moveByIndex(present, value);
+            switch (value) {
+                case 0:
+                    line(old.x * cellSize, old.y * cellSize, present.x * cellSize, present.y * cellSize);
+                    break;
+                case 1:
+                    arc((present.x - 1) * cellSize, present.y * cellSize, cellSize * 2, cellSize * 2, -PI / 2, 0);
+                    break;
+                case 2:
+                    arc(present.x * cellSize, old.y * cellSize, cellSize * 2, cellSize * 2, PI / 2, PI);
+                    break;
+                case 3:
+                    line(old.x * cellSize, old.y * cellSize, present.x * cellSize, present.y * cellSize);
+                    break;
+                case 4:
+                    arc(present.x * cellSize, old.y * cellSize, cellSize * 2, cellSize * 2, 0, PI / 2);
+                    break;
+                case 5:
+                    arc(old.x * cellSize, present.y * cellSize, cellSize * 2, cellSize * 2, PI, -PI / 2);
+                    break;
+                case 6:
+                    line(old.x * cellSize, old.y * cellSize, present.x * cellSize, present.y * cellSize);
+                    break;
+                case 7:
+                    arc(old.x * cellSize, present.y * cellSize, cellSize * 2, cellSize * 2, PI / 2, PI);
+                    break;
+                case 8:
+                    arc(present.x * cellSize, old.y * cellSize, cellSize * 2, cellSize * 2, -PI / 2, 0);
+                    break;
+                case 9:
+                    line(old.x * cellSize, old.y * cellSize, present.x * cellSize, present.y * cellSize);
+                    break;
+                case 10:
+                    arc(present.x * cellSize, old.y * cellSize, cellSize * 2, cellSize * 2, PI, -PI / 2);
+                    break;
+                case 11:
+                    arc(old.x * cellSize, present.y * cellSize, cellSize * 2, cellSize * 2, 0, PI / 2);
+            }
+
+        });
+    }
     directionAround(direction) {
         var directionFrom = {
-            "right" : [0, 2, 10],
-            "up" : [1, 3, 5],
-            "left" : [4, 6, 8],
-            "down" : [7, 9, 11]
+            "right": [0, 2, 10],
+            "up": [1, 3, 5],
+            "left": [4, 6, 8],
+            "down": [7, 9, 11]
         };
         var directionTo = {
-            "right" : [11, 0, 1],
-            "up" : [2, 3, 4],
+            "right": [11, 0, 1],
+            "up": [2, 3, 4],
             "left": [5, 6, 7],
             "down": [8, 9, 10]
         }
-        Object.keys(directionFrom).forEach((keys)=>{
-            if (directionFrom[keys].includes(direction)){
-                console.log(directionTo[keys]);
-                return (directionTo[keys]);
-            } 
-        });
-        console.log('not found!');
+        for (let key of Object.keys(directionFrom)) {
+            if (directionFrom[key].includes(direction)) {
+                var possibleDerections = directionTo[key].concat(); //must完全拷贝
+                for (let i = 0; i < directionTo[key].length; i++) {
+                    let tempPosition = {
+                        x: this.keyPt.x,
+                        y: this.keyPt.y
+                    };
+                    this.moveByIndex(tempPosition, directionTo[key][i]);
+                    if (
+                        (
+                            (directionTo[key][i] % 3 !== 0) &&
+                            (dotArray[this.keyPt.x][tempPosition.y] == true) &&
+                            (dotArray[tempPosition.x][this.keyPt.y] == true)
+                        ) ||
+                        (dotArray[tempPosition.x][tempPosition.y] == true)
+                    ) {
+                        possibleDerections.splice(possibleDerections.indexOf(directionTo[key][i]), 1);
+                    }
+
+                }
+                if (possibleDerections.length == 0) {
+                    console.log('dead');
+                    this.end = true;
+                }
+                return (possibleDerections);
+            }
+        }
+    }
+    moveByIndex(position, index) {
+        switch (index) {
+            case 0:
+                position.x++;
+                break;
+            case 1:
+                position.x++;
+                position.y++;
+                break;
+            case 2:
+                position.x++;
+                position.y++;
+                break;
+            case 3:
+                position.y++;
+                break;
+            case 4:
+                position.y++;
+                position.x--;
+                break;
+            case 5:
+                position.y++;
+                position.x--;
+                break;
+            case 6:
+                position.x--;
+                break;
+            case 7:
+                position.x--;
+                position.y--;
+                break;
+            case 8:
+                position.x--;
+                position.y--;
+                break;
+            case 9:
+                position.y--;
+                break;
+            case 10:
+                position.x++;
+                position.y--;
+                break;
+            case 11:
+                position.x++;
+                position.y--;
+        }
     }
 }
 
-var acurve = new oneCurve(0,1);
+for (let i = 0; i < number; i++) {
+    let index = Math.floor(Math.random() * bound.length);
+    let temp = new oneCurve(bound[index].x, bound[index].y);
+}
 
 function setup() {
-
+    createCanvas(window.innerWidth, window.innerHeight);
+    dotRender();
+    curves.forEach((value) => {
+        value.render();
+    })
 }
 
 function draw() {
 
+}
+
+function dotRender() {
+    for (let i = 0; i < xSize; i++) {
+        for (let j = 0; j < ySize; j++) {
+            strokeWeight(dotWidth);
+            stroke(255, 0, 0);
+            point(i * cellSize, j * cellSize);
+        }
+    }
 }
--- test.js
+++ test.js
@@ -1,10 +1,24 @@
-class Test{
-    constructor(x){
-        this.x = this.f(x);
+class Link {
+    constructor(value, status = true, possibleArray = [], choose = null) {
+        this.value = value;
+        this.status = status;
+        this.possibleArray = possibleArray;
+        this.choose = choose;
     }
-    f(x){
-        return ++x;
+    deepestChoose(l) {
+        if (l.choose == null) {
+            return l;
+        } else {
+            var next = this.deepestChoose(l.choose);
+            if (next) return next;
+        }
     }
 }
-a = new Test(10);
-console.log(a.x);
+
+var l = new Link(0);
+var i = 0;
+for (let i = 0;i<10;i++){
+    if (l.deepestChoose(l).value < 100){
+        l.choose = new Link(i++);
+    }
+}
